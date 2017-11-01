12f3ae4f438941709ca2f26dc5a9a48c1057fca9
Lochlin Craig-263005078 <263005078@vusdnoc.vusd.k12.ca.us>
2017-11-01 10:09:54 -0700

    Piece added

--- src/stratego/Piece.java
+++ src/stratego/Piece.java
@@ -1,6 +1,37 @@
 
 package stratego;
 
+import java.awt.*;
+
 public class Piece {
+    private Color color;
+    private int rank;
     
+    Piece(){
+        color=null;
+        
+    }
+    Piece(Color _color){
+        color=_color;
+        
+    }
+    public Color getColor()
+    {
+        return (color);
+    }
+    public int getRank()
+    {
+        return (rank);
+    }
+    public void draw(Graphics2D g,int row,int column,
+    int xdelta,int ydelta) {
+        g.setColor(color); 
+        g.fillRect(Window.getX(column*xdelta)+20,
+        Window.getY(row*ydelta)+20,xdelta-20,ydelta-20);
+        
+//        g.setColor(Color.white);
+//        g.setFont(new Font("Arial",Font.PLAIN,30));
+//        g.drawString("" + rank,Window.getX(column*xdelta)+30,
+//        Window.getY(row*ydelta)+40);           
+    }
 }
